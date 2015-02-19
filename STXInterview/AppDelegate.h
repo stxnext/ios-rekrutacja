c7dff46be8abb850ae6f5bbecc2b00f7e9b120dc
Dawid Żakowski <dawid.zakowski@stxnext.pl>
2015-02-19 06:58:02 +0100

    Initial Commit

---
+++ STXInterview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STXInterview
+//
+//  Created by Dawid Żakowski on 19/02/2015.
+//  Copyright (c) 2015 STX Next. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ STXInterview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STXInterview
+//
+//  Created by Dawid Żakowski on 19/02/2015.
+//  Copyright (c) 2015 STX Next. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
